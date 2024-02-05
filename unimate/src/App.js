ccbb81f4b0d5378d0621a9aa054697dd19cfb657
DavidLoveSpace <davidlovespace@Davids-MacBook-Air.local>
2024-02-05 16:24:49 -0500

    firebase config

--- unimate/src/App.js
+++ unimate/src/App.js
@@ -1,5 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBfqb1xTPc-lVeK8vN83F3wSh-9uka90_U",
+  authDomain: "unimate-f1630.firebaseapp.com",
+  projectId: "unimate-f1630",
+  storageBucket: "unimate-f1630.appspot.com",
+  messagingSenderId: "616679306573",
+  appId: "1:616679306573:web:933d3d75892c34f001f304",
+  measurementId: "G-B6MYKJY9P6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
+
 
 function App() {
   return (
