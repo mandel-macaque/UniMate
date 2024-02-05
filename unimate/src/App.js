2d64f81ada7f99b200c36b6166afb49ca08bb763
FilipLe <48429871+FilipLe@users.noreply.github.com>
2024-02-05 16:26:14 -0500

    test

--- unimate/src/App.js
+++ unimate/src/App.js
@@ -37,7 +37,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Don't Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
