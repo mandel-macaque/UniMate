5d940c34f038b629d952c3e4f4db18fc145694a9
Nhan Ton <nhantonduc@gmail.com>
2024-02-18 18:31:08 -0500

    change background color

--- src/App.tsx
+++ src/App.tsx
@@ -8,26 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className = "w-[1440px] h-[922px] bg-gradient-to-b from-cyan-500 to-blue-900">
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
