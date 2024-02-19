a0abd0975f6763348521ffce236f12097f13d507
DavidLoveSpace <davidlovespace@crc-dot1x-nat-10-239-234-3.bu.edu>
2024-02-18 19:48:59 -0500

    fix bug for back ground color

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { list_mentors } from './backend/firebase'
 
 function App() {
-  const [count, setCount] = useState(0)
   const mentors = list_mentors()
 
   return (
     <>
-      <div className = "w-[1440px] h-[922px] bg-gradient-to-b from-cyan-500 to-blue-900">
+      <div className = "w-screen h-screen bg-gradient-to-b from-cyan-500 to-blue-900">
       </div>
     </>
   )
