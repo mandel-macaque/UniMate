992829c01180d3bde1bb9185384fea9460286866
Nhan Ton <nhantonduc@gmail.com>
2024-02-18 19:41:48 -0500

    delete extra code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
  import './App.css'
 
 function App() {
-=======
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { list_mentors } from './backend/firebase'
-
-function App() {
-  const [count, setCount] = useState(0)
-  const mentors = list_mentors()
-
->>>>>>> c98de0256a858bdac59ea72965b71fca215426b8
   return (
     <>
       <div className = "w-[1440px] h-[922px] bg-gradient-to-b from-cyan-500 to-blue-900">
