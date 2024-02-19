4781b9694d19dad4cf4d99a88e20c7bf5702641d
Nhan Ton <nhantonduc@gmail.com>
2024-02-18 19:53:53 -0500

    delete unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const mentors = list_mentors()
-
   return (
     <>
       <div className = "w-screen h-screen bg-gradient-to-b from-cyan-500 to-blue-900">
