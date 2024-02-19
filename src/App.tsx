eb90d95d42ba2678b3a26053609a4f5fd3b29372
Nhan Ton <nhantonduc@gmail.com>
2024-02-18 19:37:34 -0500

    delete unecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+ import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className = "w-[1440px] h-[922px] bg-gradient-to-b from-cyan-500 to-blue-900">
+        <h1 className="w-[558px] h-[110px] text-white text-[46px] font-normal font-['Georgia'] leading-loose tracking-wide">Meet your mentors</h1>
       </div>
     </>
   )
