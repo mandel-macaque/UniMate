9b1cfbdea0041faf4821c67cde1ec6bb72cc834b
Nhan Ton <nhantonduc@gmail.com>
2024-02-18 19:47:20 -0500

    remove size limits to fit all screen for bg color

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
- import './App.css'
+import './App.css'
 
 function App() {
   return (
     <>
-      <div className = "w-[1440px] h-[922px] bg-gradient-to-b from-cyan-500 to-blue-900">
-        <h1 className="w-[558px] h-[110px] text-white text-[46px] font-normal font-['Georgia'] leading-loose tracking-wide">Meet your mentors</h1>
+      <div className = "w-screen h-screen bg-gradient-to-b from-cyan-500 to-blue-900">
       </div>
     </>
   )
