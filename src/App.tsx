af146a164e7271f70f5886a352a706c880e90800
FilipLe <48429871+FilipLe@users.noreply.github.com>
2024-02-19 09:14:21 -0500

    feat: added MVP version of user profile card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import './App.css'
-import Card from './card'
+import Card from './components/card'
+import './components/card.css'
 
 function App() {
-  const userProfile = {
-    name: 'John Doe',
-    avatar: 'https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8=', // Sample avatar image URL
-  };
-  const attributes = ['BU', 'Class of 2026', 'Computer Science', 'Vietnam', 'Topics: Math']; // Sample attributes
-
   return (
     <>
       <div className = "w-screen h-screen bg-gradient-to-b from-cyan-500 to-blue-900">
-        <div className = "w-1/2">
-          <Card userProfile={userProfile} attributes={attributes} />
-        </div>
+        <h1>HELLO</h1>
+        <h1>WELCOME</h1>
+        <Card/>
       </div>
     </>
   )
---
+++ src/components/card.tsx
@@ -0,0 +1,80 @@
+// Card component to display user profile and attributes
+const Card = () => (
+  <div className = "container">
+    <div className = "card">
+      <div className = "imgframe">
+        <img src = "https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8="></img>
+      </div>
+
+      <div className="caption"> Mathew Nunez</div>
+      <div className="gradyear"> Class of 2027</div>
+      <div className="attributeSchool">Boston University</div>
+      <div className="attributeMajor">Computer Science</div>
+      <div className="attributeTopic">Career Advice, Resume Review</div>
+
+      <div className="button-holder">
+        <div className="button">
+          Book
+        </div>
+      </div>
+
+    </div>
+
+    <div className = "card">
+      <div className = "imgframe">
+        <img src = "https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8="></img>
+      </div>
+
+      <div className="caption"> Mathew Nunez</div>
+      <div className="gradyear"> Class of 2027</div>
+      <div className="attributeSchool">Boston University</div>
+      <div className="attributeMajor">Computer Science</div>
+      <div className="attributeTopic">Career Advice, Resume Review</div>
+
+      <div className="button-holder">
+        <div className="button">
+          Book
+        </div>
+      </div>
+
+    </div>
+    <div className = "card">
+      <div className = "imgframe">
+        <img src = "https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8="></img>
+      </div>
+
+      <div className="caption"> Mathew Nunez</div>
+      <div className="gradyear"> Class of 2027</div>
+      <div className="attributeSchool">Boston University</div>
+      <div className="attributeMajor">Computer Science</div>
+      <div className="attributeTopic">Career Advice, Resume Review</div>
+
+      <div className="button-holder">
+        <div className="button">
+          Book
+        </div>
+      </div>
+
+    </div>
+    <div className = "card">
+      <div className = "imgframe">
+        <img src = "https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8="></img>
+      </div>
+
+      <div className="caption"> Mathew Nunez</div>
+      <div className="gradyear"> Class of 2027</div>
+      <div className="attributeSchool">Boston University</div>
+      <div className="attributeMajor">Computer Science</div>
+      <div className="attributeTopic">Career Advice, Resume Review</div>
+
+      <div className="button-holder">
+        <div className="button">
+          Book
+        </div>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Card;
