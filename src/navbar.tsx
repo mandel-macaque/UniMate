1af856eb000de1b1d6ee9905fb34583992523946
Bendinh <bendinh0805@gmail.com>
2024-02-21 15:06:33 -0500

    Testing navbar

---
+++ src/navbar.tsx
@@ -0,0 +1,16 @@
+// Navigation bar for website
+import React from 'react';
+
+const Navbar: React.FC = () => {
+    return (
+        <header>
+            <nav>
+                <div>
+                    Navigation Bar
+                </div>
+            </nav>
+        </header>
+    )
+};
+  
+  export default Navbar;
