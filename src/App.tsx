7b74eb47ae0cf85d42a67a00458fc7cafa3cbb89
Manuel de la Pena <mandel@microsoft.com>
2024-02-23 13:47:45 -0500

    [Layout] Foo (#1)
    Change div, I don't care about the rest.
    
    ---------
    
    Co-authored-by: Nhan Ton <nhantonduc@gmail.com>
    Co-authored-by: Bendinh <bendinh0805@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import './App.css'
-import Card from './card'
+import Navbar from './components/navbar.tsx';
+import {Route, Routes} from "react-router-dom"
+import {Mentors} from "./pages/Mentors.tsx"
 
 function App() {
-  const userProfile = {
-    name: 'John Doe',
-    avatar: 'https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8=', // Sample avatar image URL
-  };
-  const attributes = ['BU', 'Class of 2026', 'Computer Science', 'Vietnam', 'Topics: Math']; // Sample attributes
-
   return (
     <>
-      <div className = "w-screen h-screen bg-gradient-to-b from-cyan-500 to-blue-900">
-        <div className = "w-1/2">
-          <Card userProfile={userProfile} attributes={attributes} />
-        </div>
+      <div className="App">
+        <Navbar/>
+        <Routes>
+          <Route path="/"/>
+          <Route path="/mentors" element={<Mentors/>}/>
+        </Routes>
       </div>
     </>
   )
---
+++ src/pages/Mentors.tsx
@@ -0,0 +1,25 @@
+import Card from '../card.tsx'
+
+export const Mentors = () => {
+    
+    const userProfile = {
+        name: 'John Doe',
+        avatar: 'https://media.istockphoto.com/id/464988959/photo/mallard-duck-on-white-background.jpg?s=612x612&w=0&k=20&c=S1jcDuyXuoCVUaTobTrZ5f6SlscukkyheqKDHAeflW8=', // Sample avatar image URL
+      };
+    const attributes = ['BU', 'Class of 2026', 'Computer Science', 'Vietnam', 'Topics: Math']; // Sample attributes
+    
+    return (
+        <div>
+            <h1 id = "intro-container">Meet with your mentors</h1>
+            <div className = "w-1/2">
+            <Card userProfile={userProfile} attributes={attributes} />
+            <Card userProfile={userProfile} attributes={attributes} />
+            <Card userProfile={userProfile} attributes={attributes} />
+            </div>
+        </div>
+    );
+};
+
+export default Mentors
+ 
+
